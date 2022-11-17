7c81942d7fb42cf7edbe615544bdff893339a0df
LordMatalas <alarcondaniels@icloud.com>
2022-11-16 22:18:32 -0500

    Documentación archivos Admin y Datatable
    Queda por comentar el archivo Cajero

--- admin/utils/datatable.py
+++ admin/utils/datatable.py
@@ -1,12 +1,20 @@
+'''
+Este archivo tiene como finalidad ser el que construya la tabla que organiza la información sobre los productos.
+al usuario hacer el proceso de sign in, es decir, poder ingresar sus credenciales y acceder al programa.
+'''
+
+# Se importan los modulos de las subcarpetas correspondientes de la librería Kivy
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
-
+# Se importan  el módulo correspondiente de la librería pymongo y la libreria collections
 from pymongo import MongoClient
 from collections import OrderedDict
 
+# Se usa la función Builder de la librería Kivy para indicar el código con el diseño de la ventana de inicio (este
+# presenta como un string)
 Builder.load_string('''
-<DataTable>:
+<DataTable>: 
     id: main_win
     RecycleView:
         viewclass: 'CustLabel'
@@ -28,31 +36,73 @@ Builder.load_string('''
             size: self.size
             pos: self.pos
 ''')
+
+
 class DataTable(BoxLayout):
-    def __init__(self,table='', **kwargs):
+    # Representa la tabla donde se disponen los datos que caracterizan a los productos
+
+    def __init__(self, table='', **kwargs):
+        # el método __init__ se construye mediante herencia alterada
+        # Args método constructor:
+        # **kwargs: Es un párametro que permite pasar argumentos cuya longitud varía
+        # table (str): Es una cadena de caracteres vacia que va a permitir construir la tabla
         super().__init__(**kwargs)
 
         # products = self.get_products()
-        products = table
+        products = table  # Variable que se le asigna el párametro table
 
+        # A continuación, se van a construir en forma de variables, los elementos de la tabla
+        # Títulos de las columnas
+        col_titles = [k for k in
+                      products.keys()]  # Los toma de las claves del diccionario donde estan la información de los
+        # prductos, para ello se utiliza un ciclo for.
+        # Número de filas
+        rows_len = len(products[col_titles[
+            0]])  # Se hace para averiguar cuantos filas hay que construir. Primero, se utiliza el valor que le
+        # corresponde al indice 0 en la lista que guarda los nombres de las columnas, luego se asigna ese valor como
+        # clave,y por ultimo, cuenta cuantos valores le corresponden a esa clave utilizando len.
+        self.columns = len(col_titles)  # Se le asigna a self.columns, el número de títulos de columnas
 
+        # En acto seguido, se construyen las columnas y las filas
 
-        col_titles = [k for k in products.keys()]
-        rows_len = len(products[col_titles[0]])
-        self.columns = len(col_titles)
-        # print(rows_len)
+        # Primero se crea una variable table_data que corresponde a una lista vacía y tiene como próposito guardar la
+        # información de las filas y columnas
         table_data = []
-        for t in col_titles:
-            table_data.append({'text':str(t),'size_hint_y':None,'height':50,'bcolor':(.06,.45,.45,1)})
 
+        # Construcción de las columnas Se utiliza un ciclo for para recorrer todos los valores en la lista col_titles
+        # que representan los títulos de las columnas
+        for t in col_titles:
+            # Se agregan los siguientes valores en forma de diccionario a la lista table_data La clave es un valor
+            # str que corresponde a la caracteristica especifica de la columna y el valor que le corresponde
+            # 1)"text":str(t)-- Representa al texto escrito en cada columna 2)"size_hint_y": None --Representa el
+            # ancho, se le asigne a el valor None para que se pueda ajustar automaticamente según la proporción de la
+            # pantalla 3)"height":50 -- Representa la altura, el valor int es la medida de la altura 4)"bcolor":(.06,
+            # .45,.45,.1) -- Representa el color de la columna, el valor es una tupla que señala el color en código
+            # RGBa
+            table_data.append({'text': str(t), 'size_hint_y': None, 'height': 50, 'bcolor': (.06, .45, .45, 1)})
+
+        # Construcción de las filas
+        # Se utiliza un ciclo for con rango rows_len que representa el número de filas que se necesitan crear
         for r in range(rows_len):
+            # Se utiliza otro ciclo for dentro para crear las filas por cada columna
             for t in col_titles:
-                table_data.append({'text':str(products[t][r]),'size_hint_y':None,'height':30,'bcolor':(.06,.25,.25,1)})
+                # Se agregan los siguientes valores en forma de diccionario a la lista table_data La clave es un
+                # valor str que corresponde a la caracteristica especifica de la columna y el valor que le
+                # corresponde 1)"text":str(products[t][r])-- Representa al texto escrito en cada columna,
+                # se le asigna el valor que corresponde al indice del título de la columna correspondiente.
+                # 2)"size_hint_y": None --Representa el ancho, se le asigne a el valor None para que se pueda ajustar
+                # automaticamente según la proporción de la pantalla 3)"height":30 -- Representa la altura,
+                # el valor int es la medida de la altura 4)"bcolor":(.06,.25,.25,.1) -- Representa el color de la
+                # columna, el valor es una tupla que señala el color en código RGBa
+                table_data.append(
+                    {'text': str(products[t][r]), 'size_hint_y': None, 'height': 30, 'bcolor': (.06, .25, .25, 1)})
+
+        # Se le asigna a estas variables creadas en el código de kivy, estos valores para que construyan las columnas
+        # y la tabla
         self.ids.table_floor_layout.cols = self.columns
         self.ids.table_floor.data = table_data
 
-
-    
+# Aqui se puede correr el programa, utilizando este código:
 # class DataTableApp(App):
 #     def build(self):
 
--- inicio/inicio.py
+++ inicio/inicio.py
@@ -1,56 +1,127 @@
+'''
+Este archivo tiene como finalidad ser el que construya la ventana que le permita
+al usuario hacer el proceso de sign in, es decir, poder ingresar sus credenciales y acceder al programa.
+'''
+
+# Se importan los modulos de las subcarpetas correspondientes de la librería Kivy
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
-
+# Se importan  el módulo correspondiente de la librería pymongo
 from pymongo import MongoClient
 
+# Se usa la función Builder de la librería Kivy para invocar el archivo de kivy que contiene el código con el diseño
+# de la ventana de inicio
 Builder.load_file('inicio/inicio.kv')
 
+
 class VentanaInicio(BoxLayout):
+    # Representa la ventana que le permita al usuario hacer el proceso de sign in, esta se hereda de la clase
+    # BoxLayout que proviene de la librería Kivy
+
     def __init__(self, **kwargs):
+        # el método __init__ se construye mediante herencia alterada
+        # Args método constructor:
+        # **kwargs: Es un párametro que permite pasar argumentos cuya longitud varía
         super().__init__(**kwargs)
 
     def validacion_usuario(self):
-        client = MongoClient()
-        db = client.facturacion
-        users = db.users
+        # Este método tiene como fin crear el proceso donde el usuario pueda ingresar sus credenciales y que estas se
+        # puedan validar.
 
-        user = self.ids.username_field
-        pwd = self.ids.pwd_field
+        # Lo que retorna es condicionado por las credenciales que el usuario ingrese, las opciones que puede retornar
+        # son:(mas adelante se explicará esto a fondo) 1. Un campo que contenga el mensaje de "Usuario o contraseña
+        # inválidos". 2. Cambie a la ventana del modo administrador. 3. Cambie a la ventana del modo cajero.
+
+        client = MongoClient()  # client es una variable que construye un objeto de la clase MongoClient de la
+        # librería pymongo
+        db = client.facturacion  # db será la variable que invoca el párametro facturación del objeto client
+        users = db.users  # users es la variable que guardará la base de datos de los usuarios
+
+        # A continuación, se construiran variables que contienen los campos donde se ingresa la información de las
+        # credenciales del usuario
+        user = self.ids.username_field  # Campo donde se ingresa el nombre de usuario
+        pwd = self.ids.pwd_field  # Campo donde se ingresa la contraseña del usuario
+        # Esta variable es el campo que contenga el mensaje si las credenciales se invalidan
         info = self.ids.info
 
+        # En acto seguido, se crean variables que guardan el texto que contiene los campos de nombre de usuario y
+        # contraseña
         uname = user.text
         passw = pwd.text
 
+        # Se declaran los textos de los campos de nombre de usuario y contraseña como strings vacíos para que no
+        # hayan valores preestablecidos en estos campos.
         user.text = ''
         pwd.text = ''
 
-        if uname == '' or passw == '':
-            info.text = '[color=#FF0000]Por favor complete todos los campos[/color]'
-        else:
-            user = users.find_one({'user_name':uname})
+        # Se crean condicionales para saber que opción debe retornar este método de acuerdo a las credenciales
+        # ingresadas por el usuario
 
-            if user == None:
-                info.text = '[color=#FF0000]Usuario o contraseña inválidos[/color]'
+        # El primer condicional examina si los campos estan vacíos o no
+        if uname == '' or passw == '':
+            info.text = '[color=#FF0000]Por favor complete todos los campos[/color]'  # Si estan vacíos, aparecerá el
+            # campo text con el siguiente mensaje
+        else:
+            user = users.find_one({
+                'user_name': uname})  # Si no estan vacíos, se crea una variable donde se
+            # invocará el método find_one que busca dentro de los datos guardados en users el diccionario con clave
+            # user_name y el valor ingresado en el campo de nombre de usuario
+
+            # A partir del primer condicional, se establece el siguiente que mira si el nombre de usuario existe en
+            # la base datos
+            if user == None:  # Esto quiere decir que no existe en la base de datos
+                info.text = '[color=#FF0000]Usuario o contraseña inválidos[/color]'  # Si no esta el nombre de
+                # usuario en la base de datos, aparecerá el campo text con el siguiente mensaje
             else:
-                if passw == user['password']:
-                    des = user['designation']
-                    info.text = ''
-                    self.parent.parent.parent\
-                        .ids.scrn_op.children[0]\
-                            .ids.loggedin_user.text = uname
+                # Se crea otro condicional que examina si la contraseña insertada corresponde con el nombre de
+                # usuario dado.
+                if passw == user['password']:  # Es decor si el texto insertado en el campo de la contraseña coincida
+                    # con el valor
+                    # que este con la clave password del nombre de usuario
+
+                    des = user[
+                        'designation']  # Se guarda en una variable el valor que corresponde a la clave designation
+                    # del nombre de usuario insertado, es decir, el rol del usuario
+                    info.text = ''  # No se imprime ningun mensaje en este campo
+                    # En la siguiente linea, se invocan tres métodos de la clase heredada BoxLayout, estas se separan
+                    # mediante un "\". El conjunto de estas indican cual ventana tiene que ser mostrada. Nota:El
+                    # orden para leer esta linea es de derecha a izquierda 1).ids.loggedin_user es una variable input
+                    # presente en inicio.kv que guarda la información acerca del procedimiento del proceso signin del
+                    # usuario 2).ids.scrn_op.children[0] invoca la clase de kivy Children para que tome la pantalla
+                    # scrn_op como secundaria. Se le pone indice 0 porque scrn_op solo contiene una pantalla. La
+                    # información de la variable anterior se entrega a esta pantalla. 3)self.parent.parent.parent: La
+                    # función recursiva parent indica que la pantalla inicial se encuentra en el archivo "inicio.kv".
+                    # Como en este método se invocan tres pantallas (inicio, admin y cajero), se invoca este método
+                    # tres veces.
+
+                    self.parent.parent.parent \
+                        .ids.scrn_op.children[0] \
+                        .ids.loggedin_user.text = uname
+
+                    # Teniendo en cuenta que la contraseña coincida con el nombre de usuario, se crea el condicional
+                    # según el rol del usuario
                     if des == 'Administrator':
-                        self.parent.parent.current = 'scrn_admin'
+                        self.parent.parent.current = 'scrn_admin'  # Si el rol del usuario es administrador,
+                        # se ingresa a la ventana del administrador
                     else:
-                        self.parent.parent.current = 'scrn_op'
+                        self.parent.parent.current = 'scrn_op'  # Si el rol del usuario es cajero, se ingresa a la
+                        # ventana del cajero
+
                 else:
-                    info.text = '[color=#FF0000]Usuario o contraseña inválidos[/color]'
+                    info.text = '[color=#FF0000]Usuario o contraseña inválidos[/color]'  # Si la contraseña no
+                    # coincide con el nombre de usuario, aparecerá el campo text con el siguiente mensaje
 
 
 class SigninApp(App):
+    # Representa la construcción de la ventana de inicio, es decir, la de sign in
     def build(self):
-        return VentanaInicio()
+        # Este tiene como proposito construir la ventana de inicio
 
-if __name__=="__main__":
+        return VentanaInicio()  # retorna un objeto de la clase VentanaInicio
+
+
+# Aqui se puede correr el programa
+if __name__ == "__main__":
     sa = SigninApp()
     sa.run()
